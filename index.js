604745d2653b8098a79d9d926028bd0767c45801
kuusmaa <4j534j@gmail.com>
2024-02-07 11:02:01 +0200

    Created connection to database

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequlize')
+
+sequelize
+	.authenticate()
+	.then(() => {
+	console.log('Connected to the database.');
+	})
+	catch(err => {
+	console.error('Unable to connect to the database:', err);
+	});
+
+app.get("/", (req, res) => {
+    res.json({message : "Welcome to sequlize application."});
+});
+
+app.listen(3020, () => {
+    console.log('Server is running on http:localhost:3000');
+});
